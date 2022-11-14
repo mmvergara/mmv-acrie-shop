1c9249bf0f1cefbf9ba1764d8c8ca3c374a4cdd7
mmvergara <mark.jesusmanabat@gmail.com>
2022-11-14 17:58:53 +0800

    switched to vite again after learning CRA is bad

---
+++ client/src/layout/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar: React.FC = () => {
+  return <nav className='h-[40px] text-center bg-red-400'>Acrie Shop</nav>;
+};
+
+export default Navbar;
---
+++ client/src/layout/SideNavBar.tsx
@@ -0,0 +1,18 @@
+const SideNavbar: React.FC = () => {
+  return (
+    <div className='fixed top-[40px] h-screen bg-slate-600 w-[60px]'>
+      <i>a</i>a<i>b</i>
+      <i>c</i>
+      <i>d</i>
+      <i>e</i>
+    </div>
+  );
+};
+
+
+const SidebarIcon = () => {
+  
+}
+
+
+export default SideNavbar;
